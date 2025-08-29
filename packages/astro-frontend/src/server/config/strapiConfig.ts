2730e4d256de8575087af366572c96c7bbc506c6
Alessandro Vinci <alessandro.vinci@digitouch.it>
2025-08-29 11:59:13 +0200

    fix(astro-fe): failing build

--- packages/astro-frontend/src/server/config/strapiConfig.ts
+++ packages/astro-frontend/src/server/config/strapiConfig.ts
@@ -4,7 +4,7 @@ export const StrapiApiConfig = z
   .object({
     STRAPI_API_URL: z.string().default("http://localhost"),
     STRAPI_API_PORT: z.coerce.number().min(1001).default(1337),
-    STRAPI_API_TOKEN: z.string(),
+    STRAPI_API_TOKEN: z.string().default(""),
   })
   .transform((c) => ({
     strapiApiUrl: c.STRAPI_API_URL,
