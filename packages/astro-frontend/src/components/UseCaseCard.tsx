583f9679e51800ef96444dcbb321c14fa5dd74ba
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2025-09-18 17:26:05 +0200

    Feature/pin 7709 use case card (#14)
    * Create UseCaseCard.tsx
    
    * Done use case card
    
    ---------
    
    Co-authored-by: DavideTurus <davide.turus@alten.it>

---
+++ packages/astro-frontend/src/components/UseCaseCard.tsx
@@ -0,0 +1,69 @@
+import {
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardTagsHeader,
+  CardText,
+  CardTitle,
+  Chip,
+  ChipLabel,
+  Col,
+  Row,
+} from "design-react-kit";
+import React from "react";
+
+type UseCaseCardProps = {
+  title: string;
+  description: string | React.ReactNode;
+  categoryTag: string;
+  isNew?: boolean;
+  replicableBy: string[];
+};
+
+const UseCaseCard: React.FC<UseCaseCardProps> = ({
+  categoryTag,
+  isNew = false,
+  title,
+  description,
+  replicableBy,
+}) => (
+  <Row>
+    <Col lg="6" xs="12">
+      <Card className="card-bg card-big no-after" spacing>
+        <CardBody>
+          <CardTagsHeader>
+            <Chip className="my-0">
+              <ChipLabel>{categoryTag}</ChipLabel>
+            </Chip>
+            {isNew && <Badge color="success">Nuovo</Badge>}
+          </CardTagsHeader>
+          <CardTitle tag="h5">{title}</CardTitle>
+          <CardText>{description}</CardText>
+          {/* <div className="it-card-footer"> */}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "flex-start",
+              flexDirection: "column",
+            }}
+          >
+            <span>
+              <strong>Replicabile da:</strong> {replicableBy.join(", ")}
+            </span>
+            <Button
+              color="primary"
+              size="sm"
+              style={{ display: "block", marginTop: "16px" }}
+            >
+              Scopri di più
+            </Button>
+          </div>
+        </CardBody>
+      </Card>
+    </Col>
+  </Row>
+);
+
+export default UseCaseCard;
