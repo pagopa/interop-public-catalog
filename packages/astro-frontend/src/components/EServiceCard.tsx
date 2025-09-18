86e195154e844a5dddf38e403047e16c41194aa9
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2025-09-18 17:30:38 +0200

    Feature/pin 7710 eservice card (#15)
    * Create EServiceCard.tsx
    
    * Done eservice card
    
    ---------
    
    Co-authored-by: DavideTurus <davide.turus@alten.it>

---
+++ packages/astro-frontend/src/components/EServiceCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardReadMore,
+  CardTagsHeader,
+  CardText,
+  CardTitle,
+  Chip,
+  ChipLabel,
+  Col,
+  Icon,
+  Row,
+} from "design-react-kit";
+
+type EServiceCardProps = {
+  title: string | React.ReactNode;
+  description: string | React.ReactNode;
+};
+
+const EServiceCard: React.FC<EServiceCardProps> = ({ title, description }) => (
+  <Row>
+    <Col xs="3">
+      <Card className="card-bg" spacing>
+        <CardBody>
+          <CardTagsHeader>
+            <span>Nome dell'ente</span>
+            <Chip className="my-0">
+              <Icon icon="it-github" size="xs" />
+              <ChipLabel>Accesso riservato</ChipLabel>
+            </Chip>
+          </CardTagsHeader>
+          <CardTitle tag="h5">{title}</CardTitle>
+          <CardText>{description}</CardText>
+          <CardReadMore
+            href="#"
+            iconName="it-arrow-right"
+            text="Leggi di più"
+          />
+        </CardBody>
+      </Card>
+    </Col>
+  </Row>
+);
+
+export default EServiceCard;
