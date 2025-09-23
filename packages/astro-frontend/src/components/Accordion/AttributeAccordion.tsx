9478497e99522883e044e019d94a3087d25581f8
alten-dturus <davide.turus@alten.it>
2025-09-23 11:47:29 +0200

    Feature: PIN-7751 - Accordion (#20)
    * Implemented AttributeAccordionList and AttributeAccordionItem
    
    * update component exports
    
    * fix import
    
    * Delete PageLayout.tsx
    
    * Added example
    
    ---------
    
    Co-authored-by: Ruggero Castagnola <ruggerocastagnola@gmail.com>

---
+++ packages/astro-frontend/src/components/Accordion/AttributeAccordion.tsx
@@ -0,0 +1,29 @@
+import {
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+} from "design-react-kit";
+import React from "react";
+
+type AttributeAccordionProps = {
+  attributeName: string;
+  attributeDescription: string;
+  isOpen: boolean;
+  onToggle: () => void;
+};
+
+const AttributeAccordion: React.FC<AttributeAccordionProps> = ({
+  attributeName,
+  attributeDescription,
+  isOpen,
+  onToggle,
+}) => (
+  <AccordionItem>
+    <AccordionHeader active={isOpen} onToggle={onToggle}>
+      {attributeName}
+    </AccordionHeader>
+    <AccordionBody active={isOpen}>{attributeDescription}</AccordionBody>
+  </AccordionItem>
+);
+
+export default AttributeAccordion;
---
+++ packages/astro-frontend/src/components/Accordion/AttributeAccordionList.tsx
@@ -0,0 +1,37 @@
+import { Accordion } from "design-react-kit";
+import React, { useState } from "react";
+import AttributeAccordion from "./AttributeAccordion.js"; // TODO check why getting an error
+
+type AttributeAccordionListProps = {
+  attributes: Array<{ name: string; description: string }>;
+};
+
+const AttributeAccordionList: React.FC<AttributeAccordionListProps> = ({
+  attributes,
+}) => {
+  const [collapseElementOpen, setCollapseElement] = useState("");
+
+  const handleToggle = (index: string): void => {
+    if (collapseElementOpen === index) {
+      setCollapseElement("");
+    } else {
+      setCollapseElement(index);
+    }
+  };
+
+  return (
+    <Accordion>
+      {attributes.map((attribute, index) => (
+        <AttributeAccordion
+          key={index}
+          attributeName={attribute.name}
+          attributeDescription={attribute.description}
+          isOpen={collapseElementOpen === `${index}`}
+          onToggle={(): void => handleToggle(`${index}`)}
+        />
+      ))}
+    </Accordion>
+  );
+};
+
+export default AttributeAccordionList;
