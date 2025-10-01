c783d80c65eb9b172303bb2dff1afbfae7158151
DavideTurus <davide.turus@alten.it>
2025-10-01 19:42:54 +0200

    Create AttributesSection

---
+++ packages/astro-frontend/src/components/EServiceDetails/AttributesSection/AttributeAccordionGroup/AttributeAccordionGroup.tsx
@@ -0,0 +1,61 @@
+import { Accordion, Icon, UncontrolledTooltip } from 'design-react-kit'
+import React, { useRef, useState } from 'react'
+import AttributeAccordion from './AttributeAccordion.js'
+
+type AttributeAccordionsGroupProps = {
+  title: string
+  tooltipText: string
+  attributes: Array<{ name: string; description: string }>
+}
+
+const AttributeAccordionsGroup: React.FC<AttributeAccordionsGroupProps> = ({
+  title,
+  tooltipText,
+  attributes,
+}) => {
+  const [collapseElementOpen, setCollapseElement] = useState('')
+
+  const ref = useRef(null)
+
+  const handleToggle = (index: string): void => {
+    if (collapseElementOpen === index) {
+      setCollapseElement('')
+    } else {
+      setCollapseElement(index)
+    }
+  }
+
+  return (
+    <div className="d-flex flex-column p-3 rounded-3 bg-light">
+      <div className="d-flex flex-column">
+        <div className="d-flex flex-row align-items-center">
+          <h6>{title}</h6>
+          <Icon icon="it-info" size="xs" className="ms-2" ref={ref} />
+          {/* <UncontrolledTooltip placement="right" target={ref}>
+            {tooltipText}
+          </UncontrolledTooltip> */}
+        </div>
+        <p>
+          Per soddisfare questo requisito, devi possedere almeno uno degli attributi presenti nel
+          gruppo.
+        </p>
+      </div>
+      <Accordion>
+        {attributes.map((attribute, index) => (
+          <>
+            <AttributeAccordion
+              key={index}
+              attributeName={attribute.name}
+              attributeDescription={attribute.description}
+              isOpen={collapseElementOpen === `${index}`}
+              onToggle={(): void => handleToggle(`${index}`)}
+            />
+            {index < attributes.length - 1 && <p className="my-2">oppure</p>}
+          </>
+        ))}
+      </Accordion>
+    </div>
+  )
+}
+
+export default AttributeAccordionsGroup
