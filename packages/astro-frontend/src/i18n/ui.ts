297c670ccc820ac7fdad168367ba49124b9731c9
DavideTurus <davide.turus@alten.it>
2025-10-07 17:18:39 +0200

    Create ui.ts

---
+++ packages/astro-frontend/src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  it: "Italiano",
+  en: "English",
+};
+
+export const defaultLang = "it";
+
+export const ui = {
+  it: {
+    "nav.home": "Home",
+    "nav.catalog": "Catalogo",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.catalog": "Catalog",
+  },
+} as const;
