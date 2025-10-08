f6d6a9b92ec4dc5b8132a13cdbde1dac810a6c36
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-10-08 17:15:40 +0200

    TODO

--- packages/astro-frontend/src/i18n/homepage.i18n.ts
+++ packages/astro-frontend/src/i18n/homepage.i18n.ts
@@ -29,6 +29,15 @@ export const homepage = {
     'how_it_works.step_3.description':
       'Ottenuto l’accesso ai dati, puoi costruire i tuoi servizi. Lasciati ispirare dagli esempi pratici di utilizzo.',
     'how_it_works.step_3.link': 'Consulta gli esempi',
+
+    'use_cases.title': 'Lasciati ispirare dagli esempi pratici per il tuo ente',
+    'use_cases.description':
+      "Una raccolta di esempi significativi e facili da replicare di come l'interoperabilità può semplificare i processi della Pubblica Amministrazione.",
+    'use_cases.target.pa_centrali': 'PA Centrali',
+    'use_cases.target.regioni': 'Regioni',
+    'use_cases.target.comuni': 'Comuni',
+    'use_cases.target.view_all': 'Visualizza Tutti',
+    'use_cases.footer_cta': 'Esplora tutti gli esempi',
   },
   en: {
     title: 'Explore Public Administration APIs',
@@ -56,6 +65,15 @@ export const homepage = {
     'how_it_works.step_3.description':
       'Once you have access to the data, you can build your services. Get inspired by practical examples of use.',
     'how_it_works.step_3.link': 'See examples',
+
+    'use_cases.title': 'Get inspired by practical examples for your organization',
+    'use_cases.description':
+      'A collection of meaningful and easily replicable examples of how interoperability can simplify Public Administration processes.',
+    'use_cases.target.pa_centrali': 'Central Public Administrations',
+    'use_cases.target.regioni': 'Regions',
+    'use_cases.target.comuni': 'Municipalities',
+    'use_cases.target.view_all': 'View All',
+    'use_cases.footer_cta': 'Explore all examples',
   },
 } as const satisfies TranslationsMap
 
--- packages/astro-frontend/src/i18n/ui.i18n.ts
+++ packages/astro-frontend/src/i18n/ui.i18n.ts
@@ -4,9 +4,13 @@ import { buildUseTranslations } from './utils.i18n.js'
 export const ui = {
   it: {
     'actions.search': 'Cerca',
+    'actions.learnMore': 'Scopri di più',
+    'label.for': 'Per',
   },
   en: {
     'actions.search': 'Search',
+    'actions.learnMore': 'Learn more',
+    'label.for': 'For',
   },
 } as const satisfies TranslationsMap
 
