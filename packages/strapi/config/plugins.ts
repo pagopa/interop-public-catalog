1791d692b831e5f033bf35bf684afc3c5d5e2eac
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-10-10 13:00:39 +0200

    edit strapi upload-aws-s3 plugin configuration to use IRSA instead of static credentials

--- packages/strapi/config/plugins.ts
+++ packages/strapi/config/plugins.ts
@@ -2,21 +2,12 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: "local", // change to aws-s3 when ready
+      provider: "aws-s3",
       providerOptions: {
         baseUrl: env("CDN_URL"),
         rootPath: env("CDN_ROOT_PATH"),
         s3Options: {
-          credentials: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_ACCESS_SECRET"),
-          },
           region: env("AWS_REGION"),
-          params: {
-            ACL: env("AWS_ACL"),
-            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES"),
-            Bucket: env("AWS_BUCKET"),
-          },
         },
       },
       actionOptions: {
