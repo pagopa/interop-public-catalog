4d4d4432ff95a3f12500230a671ecf5fb26f7383
DavideTurus <davide.turus@alten.it>
2025-10-20 16:25:48 +0200

    Update rich-text-strapi-renderer.ts

--- packages/astro-frontend/src/utils/rich-text-strapi-renderer.ts
+++ packages/astro-frontend/src/utils/rich-text-strapi-renderer.ts
@@ -1,13 +1,89 @@
-type StrapiNode = {
-  type: 'paragraph' | 'heading' | 'list' | 'listItem' | 'link' | 'text'
-  text?: string
-  underline?: boolean
+type StrapiHeadingLevel = 1 | 2 | 3 | 4 | 5 | 6
+type StrapiListFormat = 'ordered' | 'unordered'
+
+type StrapiTextNode = {
+  type: 'text'
+  text: string
   bold?: boolean
   italic?: boolean
-  url?: string
+  underline?: boolean
+}
+
+type StrapiLinkNode = {
+  type: 'link'
+  url: string
   children?: StrapiNode[]
 }
 
-export function renderRichTextStrapiNodes(content: StrapiNode[]): string {
-  return '<p>TODO</P>'
+type StrapiParagraphNode = {
+  type: 'paragraph'
+  children?: StrapiNode[]
+}
+
+type StrapiHeadingNode = {
+  type: 'heading'
+  level?: StrapiHeadingLevel
+  children?: StrapiNode[]
+}
+
+type StrapiListItemNode = {
+  type: 'listItem'
+  children?: StrapiNode[]
+}
+
+type StrapiListNode = {
+  type: 'list'
+  format?: StrapiListFormat
+  children?: StrapiListItemNode[]
+}
+
+export type StrapiNode =
+  | StrapiParagraphNode
+  | StrapiHeadingNode
+  | StrapiListNode
+  | StrapiListItemNode
+  | StrapiLinkNode
+  | StrapiTextNode
+
+function wrapTextWithSemantics(node: StrapiTextNode): string {
+  let wrappedText = node.text
+
+  if (node.bold) {
+    wrappedText = `<strong>${wrappedText}</strong>`
+  }
+  if (node.italic) {
+    wrappedText = `<em>${wrappedText}</em>`
+  }
+  if (node.underline) {
+    wrappedText = `<u>${wrappedText}</u>`
+  }
+
+  return wrappedText
+}
+
+export function renderRichTextStrapiNodes(nodes: StrapiNode[]): string {
+  if (!nodes || nodes.length === 0) return ''
+
+  return nodes.reduce((acc, node) => {
+    switch (node.type) {
+      case 'paragraph': {
+        const innerHtml = renderRichTextStrapiNodes(node.children || [])
+
+        return `${acc}<p>${innerHtml}</p>`
+      }
+
+      case 'link': {
+        const innerHtml = renderRichTextStrapiNodes(node.children || [])
+
+        return `${acc}<a href="${node.url}">${innerHtml}</a>`
+      }
+
+      case 'text': {
+        const formattedText = wrapTextWithSemantics(node)
+        return `${acc}${formattedText}`
+      }
+      default:
+        return acc
+    }
+  }, '')
 }
