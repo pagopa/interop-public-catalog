1a59ae67d9c7abe05b43c6921f3f6a42bca69922
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-10-20 09:27:00 +0200

    wip

--- packages/astro-frontend/src/i18n/homepage.i18n.ts
+++ packages/astro-frontend/src/i18n/homepage.i18n.ts
@@ -30,14 +30,14 @@ export const homepage = {
       'Ottenuto l’accesso ai dati, puoi costruire i tuoi servizi. Lasciati ispirare dagli esempi pratici di utilizzo.',
     'how_it_works.step_3.link': 'Consulta gli esempi',
 
-    'use_cases.title': 'Lasciati ispirare dagli esempi pratici per il tuo ente',
-    'use_cases.description':
+    'good_practices.title': 'Lasciati ispirare dagli esempi pratici per il tuo ente',
+    'good_practices.description':
       "Una raccolta di esempi significativi e facili da replicare di come l'interoperabilità può semplificare i processi della Pubblica Amministrazione.",
-    'use_cases.target.pa_centrali': 'PA Centrali',
-    'use_cases.target.regioni': 'Regioni',
-    'use_cases.target.comuni': 'Comuni',
-    'use_cases.target.view_all': 'Visualizza Tutti',
-    'use_cases.footer_cta': 'Esplora tutti gli esempi',
+    'good_practices.target.pa_centrali': 'PA Centrali',
+    'good_practices.target.regioni': 'Regioni',
+    'good_practices.target.comuni': 'Comuni',
+    'good_practices.target.view_all': 'Visualizza Tutti',
+    'good_practices.footer_cta': 'Esplora tutti gli esempi',
 
     'featured_api.title': 'API in evidenza',
     'featured_api.explore_catalog': 'Esplora il catalogo',
@@ -69,14 +69,14 @@ export const homepage = {
       'Once you have access to the data, you can build your services. Get inspired by practical examples of use.',
     'how_it_works.step_3.link': 'See examples',
 
-    'use_cases.title': 'Get inspired by practical examples for your organization',
-    'use_cases.description':
+    'good_practices.title': 'Get inspired by practical examples for your organization',
+    'good_practices.description':
       'A collection of meaningful and easily replicable examples of how interoperability can simplify Public Administration processes.',
-    'use_cases.target.pa_centrali': 'Central Public Administrations',
-    'use_cases.target.regioni': 'Regions',
-    'use_cases.target.comuni': 'Municipalities',
-    'use_cases.target.view_all': 'View All',
-    'use_cases.footer_cta': 'Explore all examples',
+    'good_practices.target.pa_centrali': 'Central Public Administrations',
+    'good_practices.target.regioni': 'Regions',
+    'good_practices.target.comuni': 'Municipalities',
+    'good_practices.target.view_all': 'View All',
+    'good_practices.footer_cta': 'Explore all examples',
 
     'featured_api.title': 'Featured APIs',
     'featured_api.explore_catalog': 'Explore the catalog',
