19ae5ec5fb1c0627722275ad98e6283ee1faa053
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-10-22 13:52:27 +0200

    wip

--- packages/astro-frontend/src/i18n/ui.i18n.ts
+++ packages/astro-frontend/src/i18n/ui.i18n.ts
@@ -13,7 +13,6 @@ const breadcrumbLabels = {
     'breadcrumb_label.GOOD_PRACTICES_DETAILS': 'Dettaglio esempi pratici',
     'breadcrumb_label.LEGISLATION': 'Normativa',
     'breadcrumb_label.PRIVACY_POLICY': 'Privacy policy',
-    'breadcrumb_label.NOT_FOUND': 'Non trovato',
   },
   en: {
     'breadcrumb_label.HOME': 'Home',
@@ -25,7 +24,6 @@ const breadcrumbLabels = {
     'breadcrumb_label.GOOD_PRACTICES_DETAILS': 'Good practice details',
     'breadcrumb_label.LEGISLATION': 'Legislation',
     'breadcrumb_label.PRIVACY_POLICY': 'Privacy policy',
-    'breadcrumb_label.NOT_FOUND': 'Not found',
   },
 } as const satisfies { [K in SupportedLanguage]: Record<`breadcrumb_label.${RouteKey}`, string> }
 
@@ -70,6 +68,13 @@ export const ui = {
 
     'eservice_card.access_reserved': 'Accesso riservato',
     'eservice_card.read_more': 'leggi tutto',
+
+    'error.page_not_found.title': 'Pagina non trovata',
+    'error.page_not_found.message': 'Utilizza il menu per riprendere la navigazione',
+    'error.system_unavailable.title': 'Il sistema non è disponibile in questo momento',
+    'error.system_unavailable.message':
+      'Ci scusiamo per l’inconveniente. Ti invitiamo a riprovare più tardi.',
+    'error.back_to_home': 'Vai alla homepage',
 
     // Organization Types
     'organization_types.tutti_long': 'Tutti gli enti',
@@ -130,6 +135,12 @@ export const ui = {
 
     'eservice_card.access_reserved': 'Reserved access',
     'eservice_card.read_more': 'Read more',
+
+    'error.page_not_found.title': 'Page not found',
+    'error.page_not_found.message': 'Use the menu to continue browsing.',
+    'error.system_unavailable.title': 'The system is not available right now',
+    'error.system_unavailable.message': 'Sorry for the inconvenience. Please try again later.',
+    'error.back_to_home': 'Back to homepage',
 
     // Organization Types
     'organization_types.tutti_long': 'All entities',
