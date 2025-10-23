4e565af0f6eba2fc6a3ccb6f89e10aeea5078ad7
DavideTurus <davide.turus@alten.it>
2025-10-23 12:43:53 +0200

    Implement responsive pagination

---
+++ packages/astro-frontend/src/components/shared/Pagination/PagerMobile.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { usePagination } from './hooks/usePagination'
+import { getLangFromUrl } from '../../../i18n/utils.i18n'
+import { useUiTranslations } from '../../../i18n/ui.i18n'
+import { BootstrapItaliaIcon } from '../BootstrapItaliaIcon'
+
+type PagerMobileProps = {
+  totalCount: number
+  limit: number
+}
+
+export const PagerMobile: React.FC<PagerMobileProps> = ({ totalCount, limit }) => {
+  const { paginationProps } = usePagination(limit, totalCount)
+  const { actualPage, totalPages } = paginationProps
+
+  return (
+    <nav className="pagination-wrapper justify-content-center">
+      <ul className="pagination">
+        <li className="page-item">
+          <a
+            className={`page-link ${actualPage === 1 ? 'disabled' : ''}`}
+            onClick={() => paginationProps.onPageChange(paginationProps.actualPage - 1)}
+            role="button"
+            tabIndex={actualPage === 1 ? -1 : 0}
+            aria-hidden="true"
+          >
+            <BootstrapItaliaIcon
+              name="it-chevron-left"
+              color={`${actualPage === 1 ? 'secondary' : 'primary'}`}
+            />
+          </a>
+        </li>
+        <li className="page-item">
+          <span className="page-link" aria-current="page">
+            {actualPage}
+          </span>
+        </li>
+        <li className="page-item">
+          <span className="page-link">/</span>
+        </li>
+        <li className="page-item">
+          <span className="page-link">{totalPages}</span>
+        </li>
+        <li className="page-item">
+          <a
+            className={`page-link ${actualPage === totalPages ? 'disabled' : ''}`}
+            onClick={() => paginationProps.onPageChange(paginationProps.actualPage + 1)}
+            role="button"
+            tabIndex={actualPage === totalPages ? -1 : 0}
+          >
+            <BootstrapItaliaIcon
+              name="it-chevron-right"
+              color={`${actualPage === totalPages ? 'secondary' : 'primary'}`}
+            />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
