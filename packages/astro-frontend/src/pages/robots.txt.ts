07ec86b3395d1b798300819be119fb1942b44f65
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-10-24 12:34:34 +0200

    feat: disallow robots.txt dev

--- packages/astro-frontend/src/pages/robots.txt.ts
+++ packages/astro-frontend/src/pages/robots.txt.ts
@@ -1,16 +1,22 @@
 import type { APIRoute } from 'astro'
 
-const getRobotsTxt = (sitemapURL: URL) => `\
+const allowRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
 Allow: /
 
 Sitemap: ${sitemapURL.href}
 `
 
-export const GET: APIRoute = ({ site, request }) => {
-  const origin = site ?? new URL(request.url).origin
+const disallowRobotsTxt = () => `\
+User-agent: *
+Disallow: /
+`
+
+export const GET: APIRoute = ({ request }) => {
+  const origin = new URL(request.url).origin
   const sitemapURL = new URL('sitemap.xml', origin)
-  return new Response(getRobotsTxt(sitemapURL), {
+  const body = origin.toString().includes('dev') ? disallowRobotsTxt() : allowRobotsTxt(sitemapURL)
+  return new Response(body, {
     headers: {
       'Content-Type': 'text/plain; charset=utf-8',
     },
