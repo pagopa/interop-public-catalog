bc309bd1943bf649a71856a1240e90273822398b
borgesis95 <salvatore.borgesi@pagopa.it>
2025-10-27 11:30:10 +0100

    chore: temp removed ssr rendering

--- packages/astro-frontend/src/components/EServiceCatalog/EServiceCatalogApp.tsx
+++ packages/astro-frontend/src/components/EServiceCatalog/EServiceCatalogApp.tsx
@@ -1,19 +1,10 @@
-import type { EService } from '../../../../models/dist/types'
 import { EServiceCatalog } from './EServiceCatalog'
 import { EServiceCatalogContextProvider } from './EServiceCatalogContext'
 
-type EServiceCatalogAppProps = {
-  initialEservices: EService[]
-  totalCount: number
-}
-
-export const EServiceCatalogApp: React.FC<EServiceCatalogAppProps> = ({
-  initialEservices,
-  totalCount,
-}) => {
+export const EServiceCatalogApp: React.FC = () => {
   return (
     <EServiceCatalogContextProvider>
-      <EServiceCatalog initialEservices={initialEservices} totalCount={totalCount} />
+      <EServiceCatalog />
     </EServiceCatalogContextProvider>
   )
 }
