ffd02698d036e31667c139e4f13212ae91e75058
borgesis95 <salvatore.borgesi@pagopa.it>
2025-10-28 14:06:04 +0100

    feat: add spinner for loading purpose (#92)

--- packages/astro-frontend/src/components/EServiceCatalog/EServiceCatalog.tsx
+++ packages/astro-frontend/src/components/EServiceCatalog/EServiceCatalog.tsx
@@ -5,17 +5,25 @@ import React from 'react'
 import { useEServiceCatalogContext } from './EServiceCatalogContext'
 import { getEServices } from '../../services/catalog.services'
 import type { EService } from 'pagopa-interop-public-models'
+import { Spinner } from '../Spinner/Spinner'
+import { useUiTranslations } from '../../i18n/ui.i18n'
+import { getLangFromUrl } from '../../i18n/utils.i18n'
 
 export const EServiceCatalog: React.FC = () => {
   const [eservices, setEservices] = React.useState<EService[]>([])
+  const [isLoading, setIsLoading] = React.useState<boolean>(true)
   const [totalCount, setTotalCount] = React.useState<number>(0)
   const { eserviceActiveFilterState, applyFilters } = useEServiceCatalogContext()
+
+  const currentLanguage = getLangFromUrl(window.location.pathname)
+  const t = useUiTranslations(currentLanguage)
 
   const onFiltersApply = async () => {
     applyFilters()
   }
 
   const fetchEservices = async () => {
+    setIsLoading(true)
     const eserviceResponse = await getEServices({
       offset: eserviceActiveFilterState.offset,
       orderBy: eserviceActiveFilterState.orderBy,
@@ -26,6 +34,7 @@ export const EServiceCatalog: React.FC = () => {
 
     setEservices(eserviceResponse.items)
     setTotalCount(eserviceResponse.total)
+    setIsLoading(false)
   }
 
   React.useEffect(() => {
@@ -37,7 +46,13 @@ export const EServiceCatalog: React.FC = () => {
       <Container className="p-3">
         <EServiceCatalogFilters handleSubmitRequest={onFiltersApply} />
       </Container>
-      <EServiceCatalogItems eservices={eservices} totalCount={totalCount} />
+      {!isLoading ? (
+        <EServiceCatalogItems eservices={eservices} totalCount={totalCount} />
+      ) : (
+        <div className="p-9">
+          <Spinner label={t('spinner.loading')} />
+        </div>
+      )}
     </>
   )
 }
