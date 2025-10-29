b774483de95cba30ed3e77f60495dccefc123314
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-10-29 11:37:35 +0100

    fix: removed prerender pages

--- packages/astro-frontend/src/pages/index.html.ts
+++ packages/astro-frontend/src/pages/index.html.ts
@@ -9,8 +9,6 @@ import type { APIRoute } from 'astro'
 import { DEFAULT_LANG } from '../i18n/config.i18n'
 import { getLocalizedRoute } from '../i18n/utils.i18n'
 
-export const prerender = true
-
 export const GET: APIRoute = ({ request }) => {
   const origin = new URL(request.url).origin
   const target = new URL(getLocalizedRoute(DEFAULT_LANG, 'HOME'), origin)
