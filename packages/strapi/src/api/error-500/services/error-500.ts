5632f75bcb72b2c66eb60513144f4edf50cacab9
ruggero.castagnola <ruggerocastagnola@gmail.com>
2026-02-06 15:38:36 +0100

    Added Strapi models

---
+++ packages/strapi/src/api/error-500/services/error-500.ts
@@ -0,0 +1,7 @@
+/**
+ * 500 service
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreService("api::error-500.error-500");
---
+++ packages/strapi/src/api/macrocategory/controllers/macrocategory.ts
@@ -0,0 +1,7 @@
+/**
+ * macrocategory controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::macrocategory.macrocategory');
